4e9a41b515c589177b4e9cfd854f443cb9122645
zaazbb <zaazbb@163.com>
2016-03-25 18:58:38 +0800

    change bitbucket url in setup.py to github.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version=libzbar.version,
     packages=['libzbar'],
     package_dir={'libzbar': './libzbar'},
-    url='https://bitbucket.org/zaazbb/zbar_ctypes',
+    url='https://github.com/zaazbb/zbar_ctypes',
     author='zaazbb',
     author_email='zaazbb@163.com',
     platforms='Linux',
